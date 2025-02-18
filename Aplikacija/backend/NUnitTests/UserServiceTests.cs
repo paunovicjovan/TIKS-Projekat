f5619fea473d4f227c8dd74f6fe30615cb00bef9
ilija <ilija.nikolic@elfak.rs>
2025-02-18 14:45:44 +0100

    Pokusaj osposobljavanja testa

--- Aplikacija/backend/NUnitTests/UserServiceTests.cs
+++ Aplikacija/backend/NUnitTests/UserServiceTests.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using Microsoft.AspNetCore.Identity;
+using DataLayer.Models;
+using MongoDB.Driver;
+using DataLayer.DTOs.User;
 
 namespace NUnitTests;
 
@@ -6,15 +11,67 @@ namespace NUnitTests;
 public class UserServiceTests
 {
     private UserService _userService;
-    
+    private TokenService _fakeTokenService;
+    private Mock<IServiceProvider> _mockServiceProvider;
+    private Mock<ILogger<TokenService>> _mockLogger;
+    private Mock<IPasswordHasher<User>> _mockPasswordHasher;
+    private Mock<IMongoCollection<User>> _mockUserCollection;
+
+
     [SetUp]
     public void Setup()
     {
+        _mockLogger = new Mock<ILogger<TokenService>>();
+        _fakeTokenService = new TokenService(_mockLogger.Object);
+        _mockServiceProvider = new Mock<IServiceProvider>();
+        _mockUserCollection = new Mock<IMongoCollection<User>>();
+        _mockPasswordHasher = new Mock<IPasswordHasher<User>>();
+        _userService = new UserService(_fakeTokenService, _mockServiceProvider.Object);
     }
 
-    [Test]
-    public void Register_ValidUser_RegistersUser()
-    {
-        Assert.Pass();
-    }
+
+    // [Test]
+    // public async Task Register_ValidUser_RegistersUser()
+    // {
+    //     // Arrange
+    //     var userDto = new CreateUserDTO
+    //     {
+    //         Username = "validUsername",
+    //         Email = "user@example.com",
+    //         PhoneNumber = "123456789",
+    //         Password = "StrongPassword123"
+    //     };
+
+    //     _mockUserCollection
+    //             .Setup(u => u.Find(It.IsAny<Func<User, bool>>()))
+    //             .ReturnsAsync((User)null); // Pretpostavljamo da nema postojećeg korisnika
+
+    //     _mockPasswordHasher
+    //         .Setup(ph => ph.HashPassword(null!, userDto.Password))
+    //         .Returns("hashedPassword");
+
+    //     _mockUserCollection
+    //         .Setup(u => u.InsertOneAsync(It.IsAny<User>()))
+    //         .Returns(Task.CompletedTask);  // Simulacija uspešnog umetanja novog korisnika
+
+    //     var token = "generatedToken";
+    //     _fakeTokenService
+    //         .Setup(t => t.CreateToken(It.IsAny<User>()))
+    //         .Returns(token);
+
+    //     // Act
+    //     var result = await _userService.Register(userDto);
+
+    //     // Assert
+    //     Assert.IsInstanceOf<AuthResponseDTO>(result.Value);
+    //     var authResponse = result.Value as AuthResponseDTO;
+    //     Assert.AreEqual(userDto.Username, authResponse.Username);
+    //     Assert.AreEqual(userDto.Email, authResponse.Email);
+    //     Assert.AreEqual(userDto.PhoneNumber, authResponse.PhoneNumber);
+    //     Assert.AreEqual("User", authResponse.Role.ToString());
+    //     Assert.AreEqual(token, authResponse.Token);
+
+    //     _mockUserCollection.Verify(u => u.InsertOneAsync(It.IsAny<User>()), Times.Once);
+    //     _mockPasswordHasher.Verify(ph => ph.HashPassword(null!, userDto.Password), Times.Once);
+    // }
 }
