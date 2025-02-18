6eba0fbe628fe9cb026b1cb0c9a6ed7ac26c10b8
Jovan Paunovic <jovan.paunovic@elfak.rs>
2025-02-18 13:43:50 +0100

    Napravljen projekat za NUnit testove

---
+++ Aplikacija/backend/NUnitTests/UserServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace NUnitTests;
+
+[TestFixture]
+public class UserServiceTests
+{
+    private UserService _userService;
+    
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Register_ValidUser_RegistersUser()
+    {
+        Assert.Pass();
+    }
+}
